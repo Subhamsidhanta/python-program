8d1b57a99ab5fc9db35a967dfe67b3f6d7c500d8
Subham Sidhanta <94817723+Subhamsidhanta@users.noreply.github.com>
2025-06-27 15:11:07 +0530

    Add files via upload

---
+++ assignment1/basicOperation.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+print("The addition is:", a + b)
+print("The subtraction is:", a - b)
+print("The multiplication is:", a * b)
+print("The division is:", a / b)
